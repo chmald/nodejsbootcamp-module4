d9c35f471185d8f8759ea0d4fb42c15f5f9feffb
Christopher Maldonado <chmald@microsoft.com>
2019-10-24 13:45:01 -0500

    init

---
+++ src/components/Users.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <li :key="user.user" v-for="user in users">
+            {{ user.user }} - {{ user.job }}
+        </li>
+    </div>
+</template>
+
+<script>
+
+export default {
+    data() {
+        return {
+            users: []
+        }
+    },
+    mounted() {
+        axios.get('https://nodejsbootcampapi.azurewebsites.net/users').then(
+            res => {
+                this.users = res.data;
+        });
+    }
+}
+</script>
+
+<style>
+</style>
